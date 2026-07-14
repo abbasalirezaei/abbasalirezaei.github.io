b9605496258b39842db90babf6fecb8b162f66fa
Rezaei_79 <62241326+abbasalirezaei@users.noreply.github.com>
2026-07-14 22:15:14 +0330

    test 2

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -9,9 +9,11 @@ const About = () => {
           <div className="md:mx-4">
             <h3 className="text-2xl text-gray-800 font-bold mb-4">Who am I?</h3>
             <p className="text-sm text-gray-400 leading-6 mb-3">
-            Bachelor’s in Computer Science | Isfahan University
-\<br />
-I am a dedicated and enthusiastic computer science graduate with a deep passion for programming and a commitment to lifelong learning. Although I am at the beginning of my professional journey, I have actively engaged in intensive training and practical projects to develop a robust foundation in software development. My quick adaptability to new technologies and strong problem-solving skills enable me to tackle challenges effectively and deliver impactful solutions. Eager to grow and contribute, I am excited to bring my skills and enthusiasm to innovative projects within the tech industry.
+              Bachelor’s in Computer Science | Isfahan University
+              \<br />
+              I’m an IT and Telecommunication Specialist focused on designing, maintaining, and troubleshooting critical network infrastructure in the oil and gas industry.
+              Currently at South Zagros Oil & Gas Production Company, I manage local area networks (LAN), industrial switches, routers, and communication links including fiber, radio, and VSAT. I also work extensively with VoIP systems, Windows Server, Hyper-V, and IP CCTV.
+              My approach combines technical precision with a problem-solving mindset ensuring that telecommunication systems are reliable, secure, and efficient in demanding environments.
             </p>
 
           </div>
